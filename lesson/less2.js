bc37574b61f35adeaec77181c7b923612fc284d2
DmyP <pechenyi.d@gmail.com>
2017-08-18 16:04:33 +0300

    lesson2 basketball

---
+++ lesson/less2.js
@@ -0,0 +1,18 @@
+/*
+<script>
+var arr = [20, -33, 1, 11, 333, 111]
+
+arr.sort(function(a, b) {return a - b});
+
+arr.splice(3, 0, "privet"); //add
+//pull
+//arr.push(1000);
+//arr.pop(5); remove 5th element
+//arr.unshift(5);
+//arr.shift(5);
+
+document.write(arr.slice(1, 4)); // cut from to
+document.write(arr)
+</script>
+
+*/
